8aed5938812257b0a0268fc295577bf6354274f9
Felix Kimutai <kmtflx@gmail.com>
2025-05-25 11:24:47 +0300

    permissions added

---
+++ accounts/admin.py
@@ -0,0 +1,33 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+
+from .forms import CustomUserCreationForm, CustomUserChangeForm
+from .models import CustomUser
+
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = CustomUser
+
+    list_display = [
+            "email",
+            "username",
+            "name",
+            "is_staff",
+    ]
+    # fieldsets = UserAdmin.fieldsets + ((None, {"fields": ("name", )}),)
+    fieldsets = (
+        *UserAdmin.fieldsets,
+        (None, {"fields": ("name",)}),
+    )
+    # add_fieldsets = UserAdmin.add_fieldsets + ((None, {"fields": ("name", )}),)
+    add_fieldsets = (
+        (None, {
+            "classes": ("wide",),
+            "fields": ("username", "name", "password1", "password2"),
+        }),
+    )
+    
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
