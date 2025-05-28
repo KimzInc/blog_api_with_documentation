cc4d091c461a6689b47ec48ff4ea6c6ffeb27f3b
Felix Kimutai <kmtflx@gmail.com>
2025-05-28 23:25:17 +0300

    Removed .env and changes on wsgi file

--- django_project/wsgi.py
+++ django_project/wsgi.py
@@ -8,13 +8,17 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
+os.environ['SECRET_KEY'] = 'QKRf5WEpV5H5HzLPzF8kwqCzWl0aFkkfSU6IBvrt1T0'
+os.environ['DEBUG'] = 'False'
+os.environ['DATABASE_URL'] = 'mysql://Kimzaf:cHEZ%40na%4086@Kimzaf.mysql.pythonanywhere-services.com/Kimzaf$bloapidb'
 
+path = '/home/Kimzaf/kimzaf.pythonanywhere.com'
+if path not in sys.path:
+    sys.path.append(path)
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'django_project.settings'
 
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_project.settings')
-
 application = get_wsgi_application()
-
-
